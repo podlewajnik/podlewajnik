08066e99b4601ba3c2d05de5e5e0b51ca45864ad
Faustyna Szenborn <kot@koty.cat>
2024-07-27 15:24:42 +0200

    tests refactor, test_data added

--- podlewajnik_front/e2e/tests/register.spec.ts
+++ podlewajnik_front/e2e/tests/register.spec.ts
@@ -1,7 +1,12 @@
 import { test, expect } from '@playwright/test';
-import { generateRandomString } from '@helpers/helpers';
 import { selectors, errorSelectors, errorMessages } from '@pages/register.page';
+import { registerData } from '@test_data/register.data';
 
+const userName = registerData.userName;
+const userMail = registerData.userMail;
+const userLogin = registerData.userLogin;
+const userPassword = registerData.userPassword;
+const confirmPassword = registerData.confirmPassword;
 
 test.describe('Actions on register page', () => {
   test.beforeEach(async ({ page }) => {
@@ -9,13 +14,13 @@ test.describe('Actions on register page', () => {
   });
 
   test('Check Successful Registration', async ({ page }) => {
-    const randomLogin = generateRandomString(6, 12);
-
-    await page.getByLabel(selectors.name).fill('Jan');
-    await page.getByLabel(selectors.email).fill('jan2@jan.ioo');
-    await page.getByLabel(selectors.login).fill(randomLogin);
-    await page.getByLabel(selectors.password, { exact: true }).fill('Test1234!');
-    await page.getByLabel(selectors.confirmPassword).fill('Test1234!');
+    await page.getByLabel(selectors.name).fill(userName);
+    await page.getByLabel(selectors.email).fill(userMail);
+    await page.getByLabel(selectors.login).fill(userLogin);
+    await page
+      .getByLabel(selectors.password, { exact: true })
+      .fill(userPassword);
+    await page.getByLabel(selectors.confirmPassword).fill(confirmPassword);
     await page.getByRole('button', { name: selectors.confirmButton }).click();
 
     await expect(page).toHaveURL('/main-page');
@@ -28,7 +33,9 @@ test.describe('Actions on register page', () => {
     const emailError = await page.locator(errorSelectors.email);
     const loginError = await page.locator(errorSelectors.login);
     const passwordError = await page.locator(errorSelectors.password);
-    const confirmPasswordError = await page.locator(errorSelectors.confirmPassword);
+    const confirmPasswordError = await page.locator(
+      errorSelectors.confirmPassword,
+    );
 
     await expect(nameError).toBeVisible();
     await expect(emailError).toBeVisible();
@@ -38,14 +45,16 @@ test.describe('Actions on register page', () => {
   });
 
   test('Check Some Empty Required Fields', async ({ page }) => {
-    await page.getByLabel(selectors.name).fill('Andy');
-    await page.getByLabel(selectors.email).fill('andy@andy.ol');
+    await page.getByLabel(selectors.name).fill(userName);
+    await page.getByLabel(selectors.email).fill(userMail);
     await page.getByRole('button', { name: selectors.confirmButton }).click();
     await page.getByLabel(selectors.login).click();
 
     const loginError = await page.locator(errorSelectors.login);
     const passwordError = await page.locator(errorSelectors.password);
-    const confirmPasswordError = await page.locator(errorSelectors.confirmPassword);
+    const confirmPasswordError = await page.locator(
+      errorSelectors.confirmPassword,
+    );
 
     await expect(loginError).toBeVisible();
     await expect(passwordError).toBeVisible();
@@ -53,23 +62,29 @@ test.describe('Actions on register page', () => {
   });
 
   test('Check Invalid Email Format', async ({ page }) => {
-    await page.getByLabel(selectors.name).fill('Mary');
-    await page.getByLabel(selectors.email).fill('mary1mary.io');
-    await page.getByLabel(selectors.login).fill('Mary27');
-    await page.getByLabel(selectors.password, { exact: true }).fill('Test1234!');
-    await page.getByLabel(selectors.confirmPassword).fill('Test1234!');
+    await page.getByLabel(selectors.name).fill(userName);
+    await page.getByLabel(selectors.email).fill('jahn.john.john');
+    await page.getByLabel(selectors.login).fill(userLogin);
+    await page
+      .getByLabel(selectors.password, { exact: true })
+      .fill(userPassword);
+    await page.getByLabel(selectors.confirmPassword).fill(confirmPassword);
     await page.getByRole('button', { name: selectors.confirmButton }).click();
 
-    const emailError = await page.locator(`${errorSelectors.email}:has-text("${errorMessages.invalidEmail}")`);
+    const emailError = await page.locator(
+      `${errorSelectors.email}:has-text("${errorMessages.invalidEmail}")`,
+    );
     await expect(emailError).toBeVisible();
     await expect(emailError).toHaveText(errorMessages.invalidEmail);
   });
 
   test('Check Password and Confirm Password Match', async ({ page }) => {
-    await page.getByLabel(selectors.name).fill('Jan');
-    await page.getByLabel(selectors.email).fill('jan@jan.lop');
-    await page.getByLabel(selectors.login).fill('Jan1234');
-    await page.getByLabel(selectors.password, { exact: true }).fill('Test1234!');
+    await page.getByLabel(selectors.name).fill(userName);
+    await page.getByLabel(selectors.email).fill(userMail);
+    await page.getByLabel(selectors.login).fill(userLogin);
+    await page
+      .getByLabel(selectors.password, { exact: true })
+      .fill(userPassword);
     await page.getByLabel(selectors.confirmPassword).fill('Test1234');
     await page.getByRole('button', { name: selectors.confirmButton }).click();
 
@@ -78,30 +93,38 @@ test.describe('Actions on register page', () => {
   });
 
   test('Check Minimum Password Length', async ({ page }) => {
-    await page.getByLabel(selectors.name).fill('Jan');
-    await page.getByLabel(selectors.email).fill('jan@jan.ioo');
-    await page.getByLabel(selectors.login).fill('Janek18');
+    await page.getByLabel(selectors.name).fill(userName);
+    await page.getByLabel(selectors.email).fill(userMail);
+    await page.getByLabel(selectors.login).fill(userLogin);
     await page.getByLabel(selectors.password, { exact: true }).fill('1234');
     await page.getByLabel(selectors.confirmPassword).fill('1234');
     await page.getByRole('button', { name: selectors.confirmButton }).click();
 
-    const passwordError = await page.locator(errorSelectors.passwordErrorAdjacent);
+    const passwordError = await page.locator(
+      errorSelectors.passwordErrorAdjacent,
+    );
     await expect(passwordError).toHaveText(errorMessages.passwordLength);
   });
 
   test('Check Maximum Password Length', async ({ page }) => {
-    await page.getByLabel(selectors.name).fill('Jan');
-    await page.getByLabel(selectors.email).fill('jan@jan.ioo');
-    await page.getByLabel(selectors.login).fill('Janek18');
-    await page.getByLabel(selectors.password, { exact: true }).fill(
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-    );
-    await page.getByLabel(selectors.confirmPassword).fill(
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-    );
+    await page.getByLabel(selectors.name).fill(userName);
+    await page.getByLabel(selectors.email).fill(userMail);
+    await page.getByLabel(selectors.login).fill(userLogin);
+    await page
+      .getByLabel(selectors.password, { exact: true })
+      .fill(
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      );
+    await page
+      .getByLabel(selectors.confirmPassword)
+      .fill(
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      );
     await page.getByRole('button', { name: selectors.confirmButton }).click();
 
-    const passwordError = await page.locator(errorSelectors.passwordErrorAdjacent);
+    const passwordError = await page.locator(
+      errorSelectors.passwordErrorAdjacent,
+    );
     await expect(passwordError).toHaveText(errorMessages.passwordLength);
   });
 
@@ -117,7 +140,9 @@ test.describe('Actions on register page', () => {
     const emailError = await page.locator(errorSelectors.email);
     const loginError = await page.locator(errorSelectors.login);
     const passwordError = await page.locator(errorSelectors.password);
-    const confirmPasswordError = await page.locator(errorSelectors.confirmPassword);
+    const confirmPasswordError = await page.locator(
+      errorSelectors.confirmPassword,
+    );
 
     await expect(nameError).toBeVisible();
     await expect(emailError).toBeVisible();
@@ -127,9 +152,9 @@ test.describe('Actions on register page', () => {
   });
 });
 
-  //   test('Check The Navigation', async ({ page }) => {
-  //     await page.getByRole('img', { name: 'Back' }).click();
-  //     await page.waitForURL('/');
-  //     await expect(page).toHaveURL('/');
-  //   });
+//   test('Check The Navigation', async ({ page }) => {
+//     await page.getByRole('img', { name: 'Back' }).click();
+//     await page.waitForURL('/');
+//     await expect(page).toHaveURL('/');
+//   });
 // });
